cd5abbe1f86ea497187e47bd1226cf9900b6ba60
Melvin-viana <mjviana057@gmail.com>
2020-04-01 16:55:25 -0700

    Create Listing - Post Request

--- server/index.js
+++ server/index.js
@@ -1,25 +1,56 @@
 const express = require('express');
 const app = express();
-const {getListing} = require('../database/index.js');
+const {getListing, Listing} = require('../database/index.js');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/public/dist'));
 
 app.get('/api/intro/:id', function (req, res) {
   var id = req.params.id;
   getListing(id, function(err, result) {
     if (err) {
-      console.log('fail to get intro')
+      console.log('fail to get intro');
     } else {
-      res.send(result)
+      res.send(result);
     }
   });
 });
 
+// POST Request - Adds a record to
+app.post('/api/intro/', (req, res) => {
+  /**
+   * Schema for db.listings():
+   * {
+   *    listing: Number,
+   *    photos: array(5 to 6, urlStrings), // 0 - 6.
+   *    title: String,
+   *    description: String
+   * }
+   *
+   */
+
+  Listing.create(req.body)
+    .then((result) => {
+      res.send(result);
+      res.status(200);
+    })
+    .catch((err) => {
+      res.send(err);
+      res.status(500);
+    });
+});
+
+// Delete Request
+
+// Put Request
+
 app.get('/app.js', cors(), function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/public/dist/app.js'))
+  res.sendFile(path.join(__dirname, '../client/public/dist/app.js'));
 });
 
 let port = 3002;
