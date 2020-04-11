06e9066cdfa2d3bae5c6f6084d627df4007f7113
Melvin-viana <mjviana057@gmail.com>
2020-04-11 15:53:21 -0700

    Add photos table

--- database/createDb.js
+++ database/createDb.js
@@ -5,25 +5,30 @@ const config = {
   password: postgresPassword,
   port: 5432,
   host: 'localhost',
-  database:'airbnblisting'
 }
-const { dbConnection } = require('./index.js');
 const createDB = () => pgtools.createdb(config, 'airbnblisting')
+let dbConnection;
 
 createDB()
   .catch((err) => {
     if (err.message.includes('duplicate database')) {
       // Drop all tables here
-      return dbConnection.query(`DROP TABLE IF EXISTS listings`);
+      dbConnection = require('./index.js').dbConnection;
+      return dbConnection.query(`DROP TABLE IF EXISTS listings, photos`);
     } else {
       console.error(err);
     }
   })
   .then(() => {
-    return dbConnection.query(`CREATE TABLE listings (listingNumber SERIAL PRIMARY KEY, title text, description text, photos text[])`);
+    dbConnection = dbConnection || require('./index.js').dbConnection;
+    return dbConnection.query(`CREATE TABLE listings (listingNumber SERIAL PRIMARY KEY, title text, description text )`);
   })
   .then(() =>{
-   console.log('table done')
+   console.log('listings table created')
+   return dbConnection.query(`CREATE TABLE photos (id SERIAL PRIMARY KEY, listingNumber integer REFERENCES listings(listingNumber), photos text[] )`);
+  })
+  .then(()=>{
+    console.log('photos table created');
   })
   .catch((err) =>{
     console.error(err);
